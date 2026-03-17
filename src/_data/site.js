927f8be9379c7d225541d8f25bf240e583095364
Andy Clarke <malarkeysigninto+github@gmail.com>
2026-03-17 22:58:42 +0000

    Fix

---
+++ src/_data/site.js
@@ -0,0 +1,12 @@
+const siteUrl = process.env.SITE_URL || process.env.URL || process.env.DEPLOY_PRIME_URL || "";
+
+module.exports = {
+  name: "Ryan Knaggs Music",
+  url: siteUrl,
+  authorName: "Andy Clarke",
+  authorEmail: "andy.clarke@stuffandnonsense.co.uk",
+  telephone: "+1 480 978 5304",
+  email: "ryan@rkmproductions.com",
+  siteID: "ryan-knaggs-music",
+  copyrightOwner: "Ryan Knaggs Music"
+};
